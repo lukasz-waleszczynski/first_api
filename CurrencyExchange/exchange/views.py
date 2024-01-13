ae174b69f7b288b4b737758c44b2830185438810
lukasz-waleszczynski <l.waleszczynski@gmail.com>
2024-01-13 11:45:28 +0100

    Api ready, pushing state before opening server

--- CurrencyExchange/exchange/views.py
+++ CurrencyExchange/exchange/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Currency
 
-# Create your views here.
+def get_exchange_rate(request):
+    currency_name = request.GET.get('currency_name', '')
+    try:
+        currency = Currency.objects.get(name=currency_name)
+        return JsonResponse({'rate': currency.rate})
+    except Currency.DoesNotExist:
+        return JsonResponse({'error': 'Cuddency not found.'})
